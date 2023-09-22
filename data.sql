13e6e637c28167ccb83ee98c0188ef4ab8bb65e5
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-09-22 22:41:59 +0100

    Insert more data into animals

--- data.sql
+++ data.sql
@@ -1,5 +1,15 @@
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES
-(' Agumon', '2020-02-03', 0, TRUE, 10.23),
-('Gabumon', '2018-11-15', 2, TRUE, 8),
-('Pikachu', '2021-01-07', 1, FALSE, 15.04),
-('Devimon', '2017-05-12', 5, TRUE, 11);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES 
+        (1 'Agumon', '2020-02-03', 0, TRUE, 10.23),
+        (2 'Gabumon', '2018-11-15', 2, TRUE, 8.00),
+        (3 'Pikachu', '2021-01-07', 1, FALSE, 15.04),
+        (4 'Devimon', '2017-05-12', 5, TRUE, 11.00);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES
+    (5 'Charmander', '2020-02-08', 0, FALSE, -11, 'Unknown'),
+    (6 'Plantmon', '2021-11-15', 2, TRUE, -5.7, 'Unknown'),
+    (7 'Squirtle', '1993-04-02', 3, FALSE, -12.13, 'Unknown'),
+    (8 'Angemon', '2005-06-12', 1, TRUE, -45, 'Unknown'),
+    (9 'Boarmon', '2005-06-07', 7, TRUE, 20.4, 'Unknown'),
+    (10 'Blossom', '1998-10-13', 3, TRUE, 17, 'Unknown'),
+    (11 'Ditto', '2022-05-14', 4, TRUE, 22, 'Unknown');
